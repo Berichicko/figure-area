6d678a40a301e971584f39a7b04c6489e4a3b9e6
Aliaksandr Zasinets <aliaksandr.zasinets@gmail.com>
2022-06-18 19:44:47 +0300

    Added text output area

--- src/main/java/by/zasinets/qa/Main.java
+++ src/main/java/by/zasinets/qa/Main.java
@@ -2,13 +2,13 @@ package by.zasinets.qa;
 
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle("black",3);
-        Rectangle rectangle = new Rectangle("red",3,3);
-        Triangle triangle = new Triangle("blue",4,4);
+        Circle circle = new Circle("black", 3);
+        Rectangle rectangle = new Rectangle("red", 3, 3);
+        Triangle triangle = new Triangle("blue", 4, 4);
 
-        System.out.println(circle.calculateArea());
-        System.out.println(rectangle.calculateArea());
-        System.out.println(triangle.calculateArea());
+        System.out.println("Area of circle: " + circle.calculateArea());
+        System.out.println("Area of rectangle: " + rectangle.calculateArea());
+        System.out.println("Area of triangle: " + triangle.calculateArea());
 
     }
 }
